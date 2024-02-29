af6a36ab6ae309e318bfd85017179a31908eb91e
zimmer95 <jorgezimmer95@gmail.com>
2024-02-29 15:17:09 -0300

    modificando la toma de datos

--- front/scripts/getMovies.js
+++ front/scripts/getMovies.js
@@ -1,10 +1,16 @@
 const createMovieCard = require("./createCards.js");
-const createErrorMessage = require("./createError.js")
-const axios = require("axios")
+const createErrorMessage = require("./createError.js");
+const axios = require("axios");
+
+const movies = require("../assets/data/data.js");
+
 
 const getMovies = async () => {
     try{
-        const data =  await axios.get(`https://henry-movies-dev-sgtm.3.us-1.fl0.io/`);
+
+        const data  = movies;
+        console.log(data)
+        /* const data =  await axios.get(`https://henry-movies-dev-sgtm.3.us-1.fl0.io/`); */
         data.data.map(movie => createMovieCard(movie.title, movie.director, movie.duration, movie.poster, movie.year, movie.rate, movie.description));
 
     }catch(data){
